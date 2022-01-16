153756c611f014a219b0dfad68d1142271799111
AlexandrP88 <alexsandr@rtural.ru>
2022-01-16 18:49:12 +0500

    changed test

--- src/test/java/ru/netology/web/FormForCardDeliveryTest.java
+++ src/test/java/ru/netology/web/FormForCardDeliveryTest.java
@@ -3,10 +3,9 @@ package ru.netology.web;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
@@ -19,21 +18,26 @@ public class FormForCardDeliveryTest {
     @Test
     public void shouldSendForm() {
         Configuration.holdBrowserOpen = true;
-        Configuration.headless = true;
         open("http://localhost:9999");
         $("[data-test-id='city'] input").val("Пермь");
         $("div[class='popup__content'] span").click();
-        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DAY_OF_MONTH, +3);
+        String planningDate = generateDate(3);
         $("[data-test-id='date'] input[class='input__control']").doubleClick()
-                .val(dateFormat.format(cal.getTime())).pressTab();
+                .val(planningDate).pressTab();
         $("[data-test-id='name'] input").val("Рогозин Илья");
         $(byName("phone")).setValue("+79054715844");
         $(byClassName("checkbox__box")).click();
         $(withText("Забронировать")).click();
         $("[data-test-id='notification']").shouldHave(text("Успешно!"), Duration.ofSeconds(15));
+        $(".notification__content").shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
     }
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+
 }
 
 
